7cd3f79fba96bef76284be8188f73b0e11fe1657
Isabela da Silva Alves <108693206+Isabela-Alves@users.noreply.github.com>
2024-08-12 18:47:02 -0300

    Atualizando o App.js

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Register from './telas/Register';
 import Login from './telas/Login';
+import Home from './telas/Home';  // Importa a tela Home
+
 
 const Stack = createStackNavigator();
 
@@ -12,6 +14,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Register">
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
