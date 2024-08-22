fa7d0603f82c09c86aceefbf2a64324c5549e6b5
Isabela da Silva Alves <108693206+Isabela-Alves@users.noreply.github.com>
2024-08-22 20:18:40 -0300

    Atualizando o Edit.js

--- telas/Edit.js
+++ telas/Edit.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { View, TextInput, Button, StyleSheet, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { View, TextInput, StyleSheet, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 import { getFirestore, doc, updateDoc, getDoc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { app } from '../firebaseConfig'; 
@@ -8,16 +9,19 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 const db = getFirestore(app);
 const auth = getAuth(app);
 
+const tagsList = ['Doce', 'Salgado', 'Vegano', 'Vegetariano', 'Sem Lactose'];
+
 const Edit = ({ route, navigation }) => {
   const { recipeId } = route.params; // Recebe o ID da receita como parâmetro
   const [recipe, setRecipe] = useState({
     name: '',
-    ingredients: [],
+    ingredients: '',
     instructions: '',
     tags: [],
     imageUrl: ''
   });
   const [selectedImage, setSelectedImage] = useState('');
+  const [selectedTags, setSelectedTags] = useState([]);
 
   useEffect(() => {
     const fetchRecipe = async () => {
@@ -25,8 +29,15 @@ const Edit = ({ route, navigation }) => {
       const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
         const data = docSnap.data();
-        setRecipe(data);
+        setRecipe({
+          name: data.name,
+          ingredients: data.ingredients.join(', '), // Inicializa com dados existentes
+          instructions: data.instructions,
+          tags: data.tags,
+          imageUrl: data.imageUrl
+        });
         setSelectedImage(data.imageUrl || '');
+        setSelectedTags(data.tags || []);
       } else {
         console.log('Documento não encontrado!');
       }
@@ -44,83 +55,105 @@ const Edit = ({ route, navigation }) => {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
-    } 
+    }
   };
 
-  const uploadImage = async (uri) => {
-    const response = await fetch(uri);
-    const blob = await response.blob();
+  const handleUploadImage = async () => {
+    if (selectedImage) {
+      const response = await fetch(selectedImage);
+      const blob = await response.blob();
 
-    const imageRef = ref(getStorage(), `images/${recipeId}/${Date.now()}`);
-    await uploadBytes(imageRef, blob);
-
-    const downloadURL = await getDownloadURL(imageRef);
-    return downloadURL;
+      const storage = getStorage();
+      const storageRef = ref(storage, `images/${Date.now()}`);
+      await uploadBytes(storageRef, blob);
+      return await getDownloadURL(storageRef);
+    }
+    return null;
   };
 
-  const handleUpdate = async () => {
-    let imageUrl = recipe.imageUrl;
+  const handleUpdateRecipe = async () => {
+    try {
+      const imageUrl = await handleUploadImage();
 
-      if (selectedImage) {
-        imageUrl = await uploadImage(selectedImage);
-      }
+      await updateDoc(doc(db, 'receitas', recipeId), {
+        name: recipe.name,
+        ingredients: recipe.ingredients.split(',').map(ingredient => ingredient.trim()), // Corrige a separação dos ingredientes
+        instructions: recipe.instructions,
+        tags: selectedTags, // Usa as tags selecionadas
+        imageUrl: imageUrl || recipe.imageUrl,
+      });
 
-      const updatedRedipe = { ...recipe, imageUrl};
+      console.log('Receita atualizada com sucesso!');
+      navigation.navigate('Home');
+    } catch (error) {
+      console.error('Erro ao atualizar a receita: ', error);
+    }
+  };
 
-      const docRef = doc(db, 'receitas', recipeId);
-      await updateDoc(docRef, updatedRedipe);
-      navigation.goBack();
+  const toggleTag = (tag) => {
+    setSelectedTags(prevTags =>
+      prevTags.includes(tag)
+        ? prevTags.filter(t => t !== tag)
+        : [...prevTags, tag]
+    );
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Edite sua Receita</Text>
-      <Text style={styles.label}>Nome</Text>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>Editar Receita</Text>
+      <Text style={styles.subtitle}>Nome da Receita</Text>
       <TextInput
-        style={styles.input}
+        placeholder="Nome da Receita"
+        placeholderTextColor={'#F37E8F'}
         value={recipe.name}
-        onChangeText={(text) => setRecipe({ ...recipe, name: text })}
-      />
-      <Text style={styles.label}>Ingredientes</Text>
-      <TextInput
+        onChangeText={text => setRecipe({...recipe, name: text})}
         style={styles.input}
-        value={recipe.ingredients.join(', ')}
-        onChangeText={(text) => setRecipe({ ...recipe, ingredients: text.split(', ') })}
+      />
+      <Text style={styles.subtitle}>Ingredientes</Text>
+      <TextInput
+        placeholder="Ingredientes (separados por vírgula)"
+        value={recipe.ingredients}
+        placeholderTextColor={'#F37E8F'}
+        onChangeText={text => setRecipe({...recipe, ingredients: text})}
+        style={styles.input}
         multiline={true}
       />
-      <Text style={styles.label}>Instruções</Text>
+      <Text style={styles.subtitle}>Passo a Passo</Text>
       <TextInput
-        style={styles.input}
+        placeholder="Instruções"
         value={recipe.instructions}
-        onChangeText={(text) => setRecipe({ ...recipe, instructions: text })}
+        placeholderTextColor={'#F37E8F'}
+        onChangeText={text => setRecipe({...recipe, instructions: text})}
+        style={styles.input}
         multiline={true}
       />
-      <Text style={styles.label}>Tags</Text>
-      <TextInput
-        style={styles.input}
-        value={recipe.tags.join(', ')}
-        onChangeText={(text) => setRecipe({ ...recipe, tags: text.split(', ') })}
-      />
-      <TouchableOpacity  onPress={handleSelectImage}>
-        <Text style={styles.imagePicker}>Selecionar Imagem</Text>
+      <Text style={styles.subtitle}>Tags</Text>
+      <View style={styles.tagsContainer}>
+        {tagsList.map(tag => (
+          <TouchableOpacity
+            key={tag}
+            style={[
+              styles.tagButton,
+              selectedTags.includes(tag) && styles.selectedTagButton
+            ]}
+            onPress={() => toggleTag(tag)}
+          >
+            <Text style={styles.tagText}>{tag}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      <TouchableOpacity onPress={handleSelectImage}>
+        <Text style={styles.buttonImg}>Alterar Imagem</Text>
       </TouchableOpacity>
-      {selectedImage ? (
-        <Image source={{ uri: selectedImage }} style={styles.image} />
-        ) : (
-        recipe.imageUrl && <Image source={{ uri: recipe.imageUrl }} style={styles.image} />
-        )}
-      <TouchableOpacity onPress={handleUpdate} >
-        <Text style={styles.editButton}>Editar</Text>
+      {selectedImage && <Image source={{ uri: selectedImage }} style={styles.image} />}
+      <TouchableOpacity onPress={handleUpdateRecipe}>
+        <Text style={styles.buttonAdd}>Atualizar</Text>
       </TouchableOpacity>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    padding: 10,
-  },
   title: {
     fontSize: 32,
     textAlign: 'center',
@@ -128,12 +161,16 @@ const styles = StyleSheet.create({
     fontFamily: 'PlayfairDisplay-Regular',
     color: '#333',
   },
-  label: {
-   color: '#333',
+  subtitle: {
+    color: '#333',
     fontSize: 20,
     marginLeft: 15,
     fontFamily: 'Poppins-Regular',
     alignSelf: 'flex-start',
+  },
+  container: {
+    padding: 10,
+    backgroundColor: '#fff',
   },
   input: {
     borderWidth: 1,
@@ -143,25 +180,14 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginTop: 2,
   },
-   image: {
+  image: {
     width: 100,
     height: 100,
     marginBottom: 20,
     marginHorizontal: 15,
-    borderRadius: 10, 
-    
-  },
-  imagePicker: {
-    margin: 15,
-    color:'#333',
-    backgroundColor: '#F9E6D8',
-    padding: 8,
     borderRadius: 10,
-    textAlign: 'center',
-    fontSize: 20,
-    fontFamily: 'Poppins-SemiBold',
   },
-   editButton: {
+  buttonAdd: {
     color: '#fff',
     backgroundColor: '#F37E8F',
     padding: 8,
@@ -171,6 +197,34 @@ const styles = StyleSheet.create({
     marginHorizontal: 120,
     marginTop: 50,
     fontFamily: 'Poppins-SemiBold',
+  },
+  buttonImg: {
+    margin: 15,
+    color: '#333',
+    backgroundColor: '#F9E6D8',
+    padding: 8,
+    borderRadius: 10,
+    textAlign: 'center',
+    fontSize: 20,
+    fontFamily: 'Poppins-SemiBold',
+  },
+  tagsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    margin: 15,
+  },
+  tagButton: {
+    backgroundColor: '#F37E8F',
+    padding: 10,
+    borderRadius: 10,
+    margin: 5,
+  },
+  selectedTagButton: {
+    backgroundColor: '#A31F51',
+  },
+  tagText: {
+    color: '#fff',
+    fontFamily: 'Poppins-Regular',
   },
 });
 
