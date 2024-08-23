c941553666f535d111b7a442377ed863f0377cd9
Isabela da Silva Alves <108693206+Isabela-Alves@users.noreply.github.com>
2024-08-23 18:00:42 -0300

    Atualizando o App.js

--- App.js
+++ App.js
@@ -3,19 +3,18 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Register from './telas/Register';
 import Login from './telas/Login';
-import Home from './telas/Home';  // Importa a tela Home
+import Home from './telas/Home';  
 import Add from './telas/Add';
 import Edit from './telas/Edit';
 import Receitas from './telas/Receitas';
 import Detalhes from './telas/Detalhes';
+import Favoritos from './telas/Favoritos'; 
 import { ActivityIndicator } from 'react-native';
 import { useFonts } from 'expo-font';
-
 
 const Stack = createStackNavigator();
 
 export default function App() {
-
   const [fontsLoaded] = useFonts({
     'Poppins-Regular': require('./assets/fontes/Poppins-Regular.ttf'),
     'Poppins-SemiBold': require('./assets/fontes/Poppins-SemiBold.ttf'),
@@ -33,10 +32,11 @@ export default function App() {
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name='Add' component={Add}/>
-        <Stack.Screen name='Edit' component={Edit}/>
-        <Stack.Screen name='Receitas' component={Receitas}/>
-        <Stack.Screen name='Detalhes' component={Detalhes}/>
+        <Stack.Screen name="Add" component={Add} />
+        <Stack.Screen name="Edit" component={Edit} />
+        <Stack.Screen name="Receitas" component={Receitas} />
+        <Stack.Screen name="Detalhes" component={Detalhes} />
+        <Stack.Screen name="Favoritos" component={Favoritos} />
       </Stack.Navigator>
     </NavigationContainer>
   );
